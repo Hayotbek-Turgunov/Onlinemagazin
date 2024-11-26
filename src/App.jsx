893ecb21fa86d632a381c4901cb626df03b315e9
Hayotbek-Turgunov <hayotbekturgunov01@gmail.com>
2024-11-26 17:17:03 +0500

    Navbar add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavbarMain from "./components/Navbar/NavbarMain";
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarMain />
+    </div>
+  );
+}
+
+export default App;
